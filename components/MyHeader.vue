f05b0c4df0599634ad8f9fc4c2f586776d147a30
Rodwyn <rodwynmoreno@gmail.com>
2021-12-22 13:57:46 -0600

    Added Header component.

---
+++ components/MyHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <b-navbar>
+          <nuxt-link to="/"  class="navbar-bran text-white">My Movies</nuxt-link>
+      </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
